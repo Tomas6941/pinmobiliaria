f4d556a03fb97b0890dbd4c8b7b29206bae18a3f
tomasgnzalez <130705700+Tomas6941@users.noreply.github.com>
2026-03-10 22:40:26 -0300

    Delete js directory

--- js/script.js
+++
@@ -1,6 +0,0 @@
-document.getElementById("contactForm")
-.addEventListener("submit", function(e){
-  e.preventDefault();
-  document.getElementById("mensaje")
-  .innerText = "Consulta enviada correctamente (modo demo).";
-});
